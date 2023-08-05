69a91c8ff9ef7d716652ebe37e0387902fbd5314
Satendra Yadav <satendray019@gmail.com>
2023-08-05 23:29:33 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Porfolio_Satendra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
